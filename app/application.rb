9f4baa4e3888d616e425c406f36b83ac952499bb
Vinh Do <vinhdo07@gmail.com>
2019-03-28 02:07:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,6 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       wanted_item = Item.all.find {|item| item.name == item_name}
-      binding.pry
       resp.write wanted_item.price
       resp.status = 200
     else
