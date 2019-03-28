b5643c5fa115c5bb7d466ea09171ed64d414e1d7
Vinh Do <vinhdo07@gmail.com>
2019-03-28 02:01:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,10 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    req.path
     @@songs.each do |song|
       resp.write "#{song.title}\n"
     end
